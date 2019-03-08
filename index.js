1b4fa424e297610d08f3f596a50229daf83d7820
SPDUK <spdevuk@gmail.com>
2019-03-08 14:24:12 +0000

    refactor: use reject properly and only clone result array if no errors occur initially

--- index.js
+++ index.js
@@ -3,17 +3,18 @@
  * @param  {Array} input  An array you wish to iterate over.
  * @param  {Number} time How many miliseconds should pass between each function call.
  * @param  {Function} fn  A function to be called on each element in the array
- * @param {Number} [index] An optional index to begin the loop at, begins at 0 by default.
- * @param {Array} [result] The result array that will be returned, you can optionally pass a starting array, each element on the input array will be passed in 1 by 1 and pushed to the result array.
- * @return {Promise} Returns a promise which will resolve as a new array with the function called against each element.
+ * @param {Number} [index] Optional: An index to begin the iteration at, begins at 0 by default.
+ * @param {Array} [result] Optional: The result array that will be returned, each element on the input array will be passed in 1 by 1 and pushed to the end of this array.
+ * @return {Promise} Returns a promise which will resolve as a new array with the function called against each element in the input array.
  */
 function iterateWithTimeout(input, time, fn, index = 0, _result = []) {
   if (!Array.isArray(input)) throw new Error('Invalid type to iterate over!');
 
-  const result = [..._result];
-  if (index >= input.length)
+  if (index >= input.length) {
     throw new Error(`Index of ${index} is too high for that array ${input}`);
+  }
 
+  const result = [..._result];
   return _iterate(input, time, fn, index, result);
 }
 
@@ -25,7 +26,7 @@ function _iterate(input, time, fn, index, result) {
 
       setTimeout(() => resolve(_iterate(input, time, fn, index + 1, result)), time);
     } catch (err) {
-      Promise.reject(new Error('Error with input', input));
+      reject(new Error('Error with input', input));
     }
   });
 }
