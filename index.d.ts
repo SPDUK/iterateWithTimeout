1fee785a7708ba0074d2587b87a121f437d685e0
SPDUK <spdevuk@gmail.com>
2019-03-08 13:32:58 +0000

    feat: add iterateWithTimeout and typescript types

---
+++ index.d.ts
@@ -0,0 +1,16 @@
+/**
+ * Iterates over each element in an array and calls a function on it with a delay between each call
+ * @param  {Array} arr  An array you wish to iterate over.
+ * @param  {Number} time How many miliseconds should pass between each function call.
+ * @param  {Function} fn  A function to be called on each element in the array
+ * @param {Number} [index] An optional index to begin the loop at, begins at 0 by default.
+ * @param {Array} [result] The result array that will be returned, you can optionally pass a starting array, each element on the input array will be passed in 1 by 1 and pushed to the result array.
+ * @return {Promise} Returns a promise which will resolve as a new array with the function called against each element.
+ */
+export default function iterateWithTimeout(
+  input: array,
+  time: number,
+  fn: function,
+  index: number,
+  result: array
+): Promise<void>;
---
+++ index.js
@@ -0,0 +1,25 @@
+/**
+ * Iterates over each element in an array and calls a function on it with a delay between each call
+ * @param  {Array} arr  An array you wish to iterate over.
+ * @param  {Number} time How many miliseconds should pass between each function call.
+ * @param  {Function} fn  A function to be called on each element in the array
+ * @param {Number} [index] An optional index to begin the loop at, begins at 0 by default.
+ * @param {Array} [result] The result array that will be returned, you can optionally pass a starting array, each element on the input array will be passed in 1 by 1 and pushed to the result array.
+ * @return {Promise} Returns a promise which will resolve as a new array with the function called against each element.
+ */
+function iterateWithTimeout(input, time, fn, index = 0, result = []) {
+  if (!Array.isArray(input)) throw new Error('Invalid type to iterate over!');
+
+  return new Promise((resolve, reject) => {
+    if (index >= input.length) return resolve(result);
+    try {
+      result.push(fn(input[index]));
+
+      setTimeout(() => resolve(iterateWithTimeout(input, time, fn, index + 1, result)), time);
+    } catch (err) {
+      Promise.reject(new Error('Error with input', input));
+    }
+  });
+}
+
+module.exports = iterateWithTimeout;
